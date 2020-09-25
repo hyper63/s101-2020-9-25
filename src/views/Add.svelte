3a89ec20da3193c5a64fea8bddb36244779ff6b9
Tom Wilson <twilson63@gmail.com>
2020-09-25 14:41:49 -0400

    9-modal-actions

--- src/views/Add.svelte
+++ src/views/Add.svelte
@@ -1,19 +1,28 @@
 <script>
+  import { createEventDispatcher } from 'svelte'
+  const dispatch = createEventDispatcher()
+  let city
+  function handleSubmit() {
+    dispatch('add', { city })
+    city = ''
+  }
 </script>
 <main>
   <section>
     <h3>Add Location</h3>
-    <form>
+    <form on:submit|preventDefault={handleSubmit}>
       <div>
         <label for="city">City</label>
-        <input type="text" id="city" />
+        <input type="text" id="city" bind:value={city} />
       </div>
+      <!--
       <div>
         <label for="country">Country</label>
         <select id="country">
           <option>-- select --</option>
         </select>
       </div>
+      -->
       <div>
         <button>Add</button>
         <a href="">Cancel</a>
--- src/views/Favorites.svelte
+++ src/views/Favorites.svelte
@@ -1,11 +1,12 @@
 <script>
   import { blur } from 'svelte/transition'
-
+  import Modal from '../components/Modal.svelte'
+  import Add from './Add.svelte'
   import CityCard from '../components/CityCard.svelte'
   import { dispatch } from '../store.js'
 
   import page from 'page'
-
+  let open = false 
   let cities = ['Charleston, SC', 'New york, NY', 'San Francisco, CA']
   //let cities = []
   function changeCurrentCity(city) {
@@ -14,12 +15,21 @@
       page('/')
     }
   }
-
+  function openModal() {
+    open = true
+  }
+  function addCity({ detail }) {
+    cities = [...cities, detail.city]
+    open = false
+  }
 </script>
+<Modal {open}>
+  <Add on:add={addCity} />
+</Modal>
 <div in:blur={{delay: 1000, duration: 1000}}>
 <nav>
   <div>Favorites</div>
-  <a href="">Add</a>
+  <a on:click={openModal}>Add</a>
 </nav>
 <main >
   <section>
