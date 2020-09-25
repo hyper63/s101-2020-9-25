dae87148d0208fb4c1f77fc59b7d1185be01f722
Tom Wilson <twilson63@gmail.com>
2020-09-25 13:26:30 -0400

    5-event

---
+++ src/components/CityCard.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { createEventDispatcher } from 'svelte'
+  const dispatch = createEventDispatcher()
+
+  export let city
+
+  function handleClick() {
+    dispatch('click', { city })
+  }
+</script>
+<aside on:click={handleClick}>
+  <h3>{city}</h3>
+</aside>
