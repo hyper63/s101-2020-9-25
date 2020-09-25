073edba5122e8f067f7df5012056292bc06c7f52
Tom Wilson <twilson63@gmail.com>
2020-09-25 12:38:04 -0400

    2

--- src/views/Current.svelte
+++ src/views/Current.svelte
@@ -1,4 +1,6 @@
 <script>
+  import Weather from '../components/Weather.svelte'
+
   const weather = {
     city: 'Charleston, SC',
     temp: '80 &deg; F',
@@ -15,12 +17,7 @@
   <a href="">Favorites</a>
 </nav>
 <main>
-  <figure>
-    <img alt={weather.description} src="/icons/{weather.icon}.png" />
-  </figure>
-  <h3>{weather.city}</h3>
-  <h1>{@html weather.temp}</h1>
-  <p>{weather.description}</p>
+  <Weather {...weather} />
 </main>
 <style>
   h3 {
